c6fca4c3319d579aef8a50dc0df37f61666df14a
christianHadinata <christianhadinata2004@gmail.com>
2025-02-13 11:59:09 +0700

    fix: fix transform on hover category card

--- components/CategoryCard.tsx
+++ components/CategoryCard.tsx
@@ -11,14 +11,17 @@ export default function CategoryCard(props: CategoryTypes) {
   return (
     <Link
       href={`/category/${props.category_id}`}
-      className="group relative h-[250px] w-[186px] cursor-pointer rounded-xl transition-all hover:scale-105"
+      className="relative h-[250px] w-[186px] cursor-pointer overflow-hidden rounded-xl"
     >
-      <img
-        src={`/category/${props.category_name}-1.jpg`}
-        alt="tech"
-        className="absolute h-full w-full rounded-xl object-cover"
-      />
-      <h3 className="absolute h-full w-full py-5 text-center text-2xl font-semibold text-white">
+      <div className="absolute inset-0 z-10 overflow-hidden">
+        <img
+          src={`/category/${props.category_name}-1.jpg`}
+          alt="tech"
+          className="h-full w-full rounded-xl object-cover transition-all hover:scale-110"
+        />
+      </div>
+
+      <h3 className="absolute left-0 top-5 z-20 w-full text-center text-2xl font-semibold text-white">
         {props.category_name}
       </h3>
     </Link>
